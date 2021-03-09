ec56d52e858ced6dccc74976343cc41c6ce3b41a
jon-wehner <jonjwehner@gmail.com>
2021-03-08 16:02:20 -0800

    animating navlink reveal

--- react-app/src/components/NavBar/index.js
+++ react-app/src/components/NavBar/index.js
@@ -1,14 +1,18 @@
+import { useState } from 'react';
 import './NavBar.css'
 
 const NavBar = ({ setAuthenticated }) => {
+  const [showNavLinks, setShowNavLinks] = useState(false)
   const toggleNavLinks = () => {
-    return null
+    setShowNavLinks(!showNavLinks)
   }
   return (
-    <nav className="navbar">
-      <h1 className="navbar__logo">PantryStock</h1>
-      <i className="fas fa-bars navlinks-toggle" onClick={toggleNavLinks}/>
-      <ul className="navbar__navlinks">
+    <nav className={"navbar" + (showNavLinks ? " navbar-active" : "")}>
+      <div className="navbar__mobile">
+        <h1 className="navbar__logo">PantryStock</h1>
+        <i className="fas fa-bars navlinks-toggle" onClick={toggleNavLinks}/>
+      </div>
+      <ul className={"navbar__navlinks" + (showNavLinks ? " navlinks-active" : "")} >
         <li className="navlink">Nav</li>
         <li className="navlink">Links</li>
         <li className="navlink">Go</li>
