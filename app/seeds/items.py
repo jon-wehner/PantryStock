63d50796d59f9efa91d8c0d74fe52d413a225475
jon-wehner <jonjwehner@gmail.com>
2021-03-12 09:32:05 -0800

    item seeds finished

--- app/seeds/items.py
+++ app/seeds/items.py
@@ -18,7 +18,7 @@ def seed_items():
         Item(name='Corn', aisle='Produce', fridge=True),
         Item(name='Garlic', aisle='Produce', fridge=False),
         Item(name='Grapes', aisle='Produce', fridge=True),
-        Item(name='Onions', aisle='Produce', fridge=False),
+        Item(name='Onion', aisle='Produce', fridge=False),
         Item(name='Pineapple', aisle='Produce', fridge=True),
         Item(name='Potoato', aisle='Produce', fridge=False),
         Item(name='Tomato', aisle='Produce', fridge=False),
@@ -30,35 +30,35 @@ def seed_items():
         Item(name='Shrimp', aisle='Seafood', fridge=True),
         Item(name='Sliced Turkey', aisle='Deli', fridge=True),
         Item(name='Pasta', aisle='Grains & Bread', fridge=False),
-        Item(name='Flour', aisle='Grains & Bread', fridge=False),
+        Item(name='Flour', aisle='Baking & Cooking', fridge=False),
         Item(name='Bread', aisle='Grains & Bread', fridge=False),
         Item(name='Breakfast Cereal', aisle='Grains & Bread', fridge=False),
         Item(name='Rice', aisle='Grains & Bread', fridge=False),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
-        Item(name='', aisle='', fridge=''),
+        Item(name='Butter', aisle='Dairy', fridge=True),
+        Item(name='Sliced American Cheese', aisle='Dairy', fridge=True),
+        Item(name='Shredded Cheddar Cheese', aisle='Dairy', fridge=True),
+        Item(name='Milk', aisle='Dairy', fridge=True),
+        Item(name='Greek Yogurt', aisle='Dairy', fridge=True),
+        Item(name='Eggs', aisle='Dairy', fridge=True),
+        Item(name='Beer', aisle='Beer & Wine', fridge=True),
+        Item(name='Wine', aisle='Beer & Wine', fridge=False),
+        Item(name='Olive Oil', aisle='Baking & Cooking', fridge=False),
+        Item(name='Salt', aisle='Baking & Cooking', fridge=False),
+        Item(name='Canned Beans', aisle='Canned Goods', fridge=False),
+        Item(name='Canned Tomatoes', aisle='Canned Goods', fridge=False),
+        Item(name='Coffee Beans', aisle='Coffee & Tea', fridge=False),
+        Item(name='Ketchup', aisle='Condiments', fridge=True),
+        Item(name='Mustard', aisle='Condiments', fridge=True),
+        Item(name='Mayonnaise', aisle='Condiments', fridge=True),
+        Item(name='Black Pepper', aisle='Baking & Cooking', fridge=False),
+        Item(name='Chicken Noodle Soup', aisle='Canned Goods', fridge=False),
+        Item(name='Canned Tuna', aisle='Canned Goods', fridge=False),
+        Item(name='Vinegar', aisle='Baking & Cooking', fridge=False),
+        Item(name='Sugar', aisle='Baking & Cooking', fridge=False),
     ]
 
-    db.session.add(demo)
-
+    for item in items:
+        db.session.add(item)
     db.session.commit()
 
 
@@ -67,5 +67,5 @@ def seed_items():
 # TRUNCATE Removes all the data from the table, and resets
 # the auto incrementing primary key
 def undo_items():
-    db.session.execute('TRUNCATE users;')
+    db.session.execute('TRUNCATE items;')
     db.session.commit()
