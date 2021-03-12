1c9179260536d8ffd0b3f143b831d0a77fcbff97
jon-wehner <jonjwehner@gmail.com>
2021-03-11 20:23:45 -0800

    unrelated styling commit because i am bad at git

--- react-app/src/LandingPage/index.js
+++ react-app/src/LandingPage/index.js
@@ -4,12 +4,12 @@ export default function LandingPage() {
   return (
     <>
       <div className="landpage__textbox">
-        <h1 className="landpage__title">Take control of your pantry</h1>
-        <p className="landpage__text">Reducing food waste at home by tracking your food from the grocery store shelves to your plate.</p>
+        <h1 className="landpage__title">Take Control of Your Pantry</h1>
+        <p className="landpage__text">Reducing food waste tracking your food from the grocery store shelves to your plate</p>
       </div>
       <div className="landingpage__signup">
         <div classname="fieldset__container">
-          <label>Take control of your pantry today!</label>
+          <label>Take control today!</label>
           <input classname="form__textfield" placeholder="Enter Email"/>
         </div>
         <button className="signup__button  button">Get Started</button>
