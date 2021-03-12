bed20d914447b32bb9885711032569f2727b1801
jon-wehner <jonjwehner@gmail.com>
2021-03-12 08:58:47 -0800

    adding item seeds

--- app/models/item.py
+++ app/models/item.py
@@ -2,7 +2,7 @@ from .db import db
 
 
 class Item(db.Model):
-    __tablename__ = 'items'
+    __tablename__ = "items"
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(), nullable=False, unique=True)
