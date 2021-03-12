e26bfd0b7d140312229ff89046fd067f8838758e
jon-wehner <jonjwehner@gmail.com>
2021-03-11 20:10:38 -0800

    more styling

--- react-app/src/LandingPage/index.js
+++ react-app/src/LandingPage/index.js
@@ -5,11 +5,11 @@ export default function LandingPage() {
     <>
       <div className="landpage__textbox">
         <h1 className="landpage__title">Take control of your pantry</h1>
-        <p className="landpage__text">Reducing foood waste at home by tracking your food from the grocery store shelves to your plate.</p>
+        <p className="landpage__text">Reducing food waste at home by tracking your food from the grocery store shelves to your plate.</p>
       </div>
       <div className="landingpage__signup">
         <div classname="fieldset__container">
-          <label classname="form__label">Take control of your pantry today!</label>
+          <label>Take control of your pantry today!</label>
           <input classname="form__textfield" placeholder="Enter Email"/>
         </div>
         <button className="signup__button  button">Get Started</button>
