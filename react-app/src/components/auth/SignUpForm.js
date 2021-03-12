0fd531c1b16f94dc3a28665a2bbb42742e0432ce
jon-wehner <jonjwehner@gmail.com>
2021-03-12 12:32:40 -0800

    form styling

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,11 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Redirect, useLocation } from 'react-router-dom';
 import { signUp } from '../../services/auth';
+import './SignUpForm.css'
 
 const SignUpForm = ({authenticated, setAuthenticated}) => {
   const location = useLocation()
   const [username, setUsername] = useState("");
-  const [email, setEmail] = useState(location.state.tempEmail);
+  const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [repeatPassword, setRepeatPassword] = useState("");
 
@@ -20,61 +21,48 @@ const SignUpForm = ({authenticated, setAuthenticated}) => {
     }
   };
 
-  const updateUsername = (e) => {
-    setUsername(e.target.value);
-  };
-
-  const updateEmail = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const updatePassword = (e) => {
-    setPassword(e.target.value);
-  };
-
-  const updateRepeatPassword = (e) => {
-    setRepeatPassword(e.target.value);
-  };
-
   if (authenticated) {
     return <Redirect to="/" />;
   }
 
   return (
     <form onSubmit={onSignUp}>
-      <div>
+      <div className="signup__fieldset">
         <label>User Name</label>
         <input
           type="text"
           name="username"
-          onChange={updateUsername}
+          onChange={e => setUsername(e.target.value)}
           value={username}
         ></input>
       </div>
-      <div>
+      <div className="signup__fieldset">
         <label>Email</label>
         <input
           type="text"
           name="email"
-          onChange={updateEmail}
+          autoComplete="username"
+          onChange={e => setEmail(e.target.value)}
           value={email}
         ></input>
       </div>
-      <div>
+      <div className="signup__fieldset">
         <label>Password</label>
         <input
           type="password"
           name="password"
-          onChange={updatePassword}
+          autoComplete="new-password"
+          onChange={e => setPassword(e.target.value)}
           value={password}
         ></input>
       </div>
-      <div>
+      <div className="signup__fieldset">
         <label>Repeat Password</label>
         <input
           type="password"
           name="repeat_password"
-          onChange={updateRepeatPassword}
+          autoComplete="new-password"
+          onChange={e => setRepeatPassword(e.target.value)}
           value={repeatPassword}
           required={true}
         ></input>
