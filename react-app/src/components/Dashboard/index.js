919a54441703c3f5b8193f3582f679383b62b9ee
jon-wehner <jonjwehner@gmail.com>
2021-03-17 16:26:39 -0700

    working on layout

---
+++ react-app/src/components/Dashboard/index.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { loadCategories, loadItems } from '../../store/items'
+
+export default function Dashboard() {
+  const dispatch = useDispatch()
+  const user = useSelector(state => state.session.user)
+
+  useEffect(() => {
+    dispatch(loadItems())
+    dispatch(loadCategories())
+  }, [dispatch])
+
+  return (
+    <h1>hi {user && user.username}!</h1>
+  )
+}
--- react-app/src/services/auth.js
+++ react-app/src/services/auth.js
@@ -1,8 +1,1 @@
-export const authenticate = async() => {
-  const response = await fetch('/api/auth/',{
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
-  return await response.json();
-}
+
