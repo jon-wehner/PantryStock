ad1b4f8f544ebdc8313550c3288ed530a3a3f0ac
jon-wehner <jonjwehner@gmail.com>
2021-03-19 16:16:40 -0700

    redux thunk working

--- react-app/src/store/shoppingList.js
+++ react-app/src/store/shoppingList.js
@@ -1,9 +1,29 @@
 const initialState = {}
 
 const createShoppingList = (name, userId) => async (dispatch) => {
-  const options
-  const shoppingList = await fetch('/')
-}
+  formData = new FormData()
+  formData.append('name', name)
+  formData.append('user_id', userId)
+  const options = {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: formData,
+  }
+  try {
+    const res = await fetch('/api/shopping-lists/', options);
+    if (!res.ok) throw res
+    const shoppingList = await res.json()
+    if(!shoppingList.errors) {
+      dispatch()
+    }
+    return shoppingList
+  }
+  catch (err){
+    return err;
+  }
+};
 
 const shoppingListReducer = (state = initialState, action) => {
   let newState;
