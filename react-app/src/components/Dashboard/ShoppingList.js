cb0fa99288dba96c950b1468c68c455d45fb83f6
jon-wehner <jonjwehner@gmail.com>
2021-03-19 23:17:50 -0700

    front end buttons working

--- react-app/src/components/Dashboard/ShoppingList.js
+++ react-app/src/components/Dashboard/ShoppingList.js
@@ -1,13 +1,31 @@
 import './Dashboard.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEdit, faTrashAlt } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faTrashAlt, faSave } from '@fortawesome/free-solid-svg-icons'
+import { useState } from 'react'
 
 export default function ShoppingList ({shoppingList}) {
+  const [edit, setEdit] = useState(false)
+  const [name, setName] = useState(shoppingList.name)
+
+const showInput = e => {
+  e.preventDefault()
+  setEdit(true)
+}
+
+const updateName = e => {
+  setName(e.target.value)
+}
+
+const saveShoppingList = e => {
+  e.preventDefault()
+  setEdit(false)
+}
+
   return (
     <div className="shoppingList">
-      <p>{shoppingList.name}</p>
+      {edit ? <input value={name} onChange={updateName}></input> : <p>{shoppingList.name}</p>}
       <div style={{ marginLeft: '5rem'}}>
-        <button className="shoppingList__buttons"><FontAwesomeIcon icon={faEdit} /></button>
+        <button className="shoppingList__buttons" onClick={edit ? saveShoppingList : showInput}><FontAwesomeIcon icon={edit ? faSave : faEdit} /></button>
         <button style={{color : 'red'}}className="shoppingList__buttons"><FontAwesomeIcon icon={faTrashAlt} /></button>
       </div>
 
