ec3dab9145774f7ea93caf60b144ffc1c5240b0b
jon-wehner <jonjwehner@gmail.com>
2021-03-22 15:57:57 -0700

    pantry python form

---
+++ app/forms/pantry_item_form.py
@@ -0,0 +1,12 @@
+from flask_wtf import FlaskForm
+from wtforms import IntegerField, DateField, FloatField
+from wtforms.validators import DataRequired
+
+
+class PantryItemForm(FlaskForm):
+    item_id = IntegerField('item_id', validators=[DataRequired()])
+    user_id = IntegerField('user_id', validators=[DataRequired()])
+    expiration_date = DateField('expiration_date', validators=[DataRequired()])
+    quantity = FloatField('quantity', validators=[DataRequired()])
+    measurement_id = IntegerField('measurement_id',
+                                  validators=[DataRequired()])
