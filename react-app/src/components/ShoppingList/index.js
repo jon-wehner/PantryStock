a87a023b39011764ae5c20f9a4ff921ed2a700ef
jon-wehner <jonjwehner@gmail.com>
2021-03-25 10:22:53 -0700

    cart to inventory working

--- react-app/src/components/ShoppingList/index.js
+++ react-app/src/components/ShoppingList/index.js
@@ -1,19 +1,41 @@
 import { useEffect } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import { useParams } from "react-router"
-import { loadOneShoppingList } from "../../store/shoppingList"
+import { addItemToInventory } from "../../store/inventory"
+import { deleteShoppingListItem, loadOneShoppingList } from "../../store/shoppingList"
 import SearchBar from "../SearchBar/SearchBar"
 import ShoppingListRow from "./ShoppingListRow"
 import './styles/ShoppingList.css'
 
 export default function ShoppingList() {
   const dispatch = useDispatch()
-  const { id } = useParams()
-  const list = useSelector(state => state.shoppingLists[id])
+  const { id: shoppingListId } = useParams()
+  const list = useSelector(state => state.shoppingLists[shoppingListId])
 
   useEffect(() => {
-    dispatch(loadOneShoppingList(id))
-  },[dispatch, id])
+    dispatch(loadOneShoppingList(shoppingListId))
+  },[dispatch, shoppingListId])
+
+  const transferList = async () => {
+    const itemsInCart = []
+    list.items.forEach(item => {
+      if (item.inCart) {
+        itemsInCart.push(item);
+      }
+    });
+    await itemsInCart.forEach(item => {
+      item.userId = list.userId
+      item.itemId = item.item.id
+      delete item.item
+      item.measurementId = item.measurement.id
+      delete item.measurement
+
+      dispatch(addItemToInventory(item))
+    })
+    await itemsInCart.forEach(item => {
+      dispatch(deleteShoppingListItem(item.id, shoppingListId))
+    })
+  }
 
   return (
     <div className="shoppingList__container">
@@ -24,6 +46,7 @@ export default function ShoppingList() {
           <ul>
             {list.items.map(row => <ShoppingListRow key={row.id} row={row} />)}
           </ul>
+          <button onClick={transferList}>Add to Inventory</button>
         </div>
       }
     </div>
