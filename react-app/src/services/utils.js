b2c082678e8742dd396ce309db8c0868c0f3aaae
jon-wehner <jonjwehner@gmail.com>
2021-04-20 06:34:42 -0700

    fixed import issue

--- react-app/src/services/utils.js
+++ react-app/src/services/utils.js
@@ -4,14 +4,14 @@ const splitDate = (date) => {
   return arr
 };
 
-const getDaysToExpire = (date) => {
+export const getDaysToExpire = (date) => {
   const expiration = new Date(date)
   const daysToExpire = Math.ceil((expiration - Date.now())/(1000*60*60*24))
 
   return daysToExpire
 };
 
-const getExpirationString = (date) => {
+export const getExpirationString = (date) => {
   const days = getDaysToExpire(date)
 
     if (days > 14) {
@@ -37,23 +37,16 @@ const getExpirationString = (date) => {
     }
 }
 
-const getTimeStamp = (date) => {
+export const getTimeStamp = (date) => {
   const dateArr = splitDate(date)
   const expiration = new Date(...dateArr)
   return expiration.toISOString()
 }
 
-const getQuantityString = (quantity, measurement) => {
+export const getQuantityString = (quantity, measurement) => {
   let quantityString = " " + measurement
   if (measurement !== "Each" && quantity > 1) {
     quantityString += 's'
   }
   return quantityString += " of "
 }
-
-module.exports = {
-  getDaysToExpire,
-  getTimeStamp,
-  getExpirationString,
-  getQuantityString
-};
