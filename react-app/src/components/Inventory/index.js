abfe748e1acbae77401c477a6ea4f5944ad0f4d7
Jon Wehner <jonjwehner@gmail.com>
2021-05-18 12:53:09 -0700

    styling cleanup

--- react-app/src/components/Inventory/index.js
+++ react-app/src/components/Inventory/index.js
@@ -2,58 +2,63 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 import { getUserInventory } from '../../store/inventory';
-import InventoryCategory from './InventoryCategory'
+import InventoryCategory from './InventoryCategory';
 
 import SearchBar from '../SearchBar/SearchBar';
-import './styles/Inventory.css'
+import './styles/Inventory.css';
 import { loadCategories } from '../../store/items';
 
 export default function Inventory() {
   const dispatch = useDispatch();
-  const {id: userId} = useParams()
+  const { id: userId } = useParams();
 
-  const [loaded, setLoaded] = useState(false)
+  const [loaded, setLoaded] = useState(false);
 
-  const categories = useSelector(state => state.items.categories);
-  const fridge = useSelector(state => state.inventory.fridge);
-  const pantry = useSelector(state => state.inventory.pantry);
-
-
+  const categories = useSelector((state) => state.items.categories);
+  const fridge = useSelector((state) => state.inventory.fridge);
+  const pantry = useSelector((state) => state.inventory.pantry);
 
   useEffect(() => {
-    dispatch(getUserInventory(userId))
+    dispatch(getUserInventory(userId));
 
-    if(!categories) {
-      dispatch(loadCategories())
+    if (!categories) {
+      dispatch(loadCategories());
+    } else {
+      setLoaded(true);
     }
-    else {
-      setLoaded(true)
-    }
-  },[dispatch, userId, categories])
+  }, [dispatch, userId, categories]);
 
-  if(!loaded) return null
+  if (!loaded) return null;
   return (
     <div className="pantry">
       <h1>Welcome to your pantry!</h1>
       <SearchBar inventory={true} />
-      <h2>Refrigerator/Freezer</h2>
-      {fridge &&
-          <section>
-            {categories.map(category => {
-              const categoryItems = fridge.filter(fridgeItem => fridgeItem.item.categoryId === category.id)
-              return <InventoryCategory key={category.id} category={category} items={categoryItems} />
-            })}
-          </section>
-      }
-      <h2>Pantry</h2>
-        {pantry &&
-          <section>
-            {categories.map(category => {
-            const categoryItems = pantry.filter(pantryItem => pantryItem.item.categoryId === category.id)
-            return <InventoryCategory key={category.id} category={category} items={categoryItems} />
+      {fridge && (
+        <section>
+          {fridge.length ? <h2>Refrigerator/Freezer</h2> : <h2>Your refrigerator is empty.</h2>}
+          {categories.map((category) => {
+            const categoryItems = fridge.filter(
+              (fridgeItem) => fridgeItem.item.categoryId === category.id
+            );
+            return (
+              <InventoryCategory key={category.id} category={category} items={categoryItems} />
+            );
           })}
-          </section>
-        }
+        </section>
+      )}
+      {pantry && (
+        <section>
+          {pantry.length ? <h2>Pantry</h2> : <h2>Your pantry is empty.</h2>}
+          {categories.map((category) => {
+            const categoryItems = pantry.filter(
+              (pantryItem) => pantryItem.item.categoryId === category.id
+            );
+            return (
+              <InventoryCategory key={category.id} category={category} items={categoryItems} />
+            );
+          })}
+        </section>
+      )}
     </div>
-  )
-};
+  );
+}
--- react-app/src/components/ShoppingList/index.js
+++ react-app/src/components/ShoppingList/index.js
@@ -1,71 +1,72 @@
-import { useEffect, useState } from "react"
-import { useDispatch, useSelector } from "react-redux"
-import { useParams } from "react-router"
-import { addItemToInventory } from "../../store/inventory"
-import { loadCategories } from "../../store/items"
-import { deleteShoppingListItem, loadOneShoppingList } from "../../store/shoppingList"
-import SearchBar from "../SearchBar/SearchBar"
-import ShoppingListCategory from "./ShoppingListCategory"
-import './styles/ShoppingList.css'
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router';
+import { addItemToInventory } from '../../store/inventory';
+import { loadCategories } from '../../store/items';
+import { deleteShoppingListItem, loadOneShoppingList } from '../../store/shoppingList';
+import SearchBar from '../SearchBar/SearchBar';
+import ShoppingListCategory from './ShoppingListCategory';
+import './styles/ShoppingList.css';
 
 export default function ShoppingList() {
-  const dispatch = useDispatch()
-  const { id: shoppingListId } = useParams()
-  const [loaded, setLoaded] = useState(false)
-  const categories = useSelector(state => state.items.categories);
-  const list = useSelector(state => state.shoppingLists[shoppingListId])
+  const dispatch = useDispatch();
+  const { id: shoppingListId } = useParams();
+  const [loaded, setLoaded] = useState(false);
+  const categories = useSelector((state) => state.items.categories);
+  const list = useSelector((state) => state.shoppingLists[shoppingListId]);
 
   useEffect(() => {
-    dispatch(loadOneShoppingList(shoppingListId))
-    if(!categories) {
-      dispatch(loadCategories())
+    dispatch(loadOneShoppingList(shoppingListId));
+    if (!categories) {
+      dispatch(loadCategories());
+    } else {
+      setLoaded(true);
     }
-    else {
-      setLoaded(true)
-    }
-  },[dispatch, shoppingListId, categories])
+  }, [dispatch, shoppingListId, categories]);
 
   const transferList = async () => {
-    const itemsInCart = []
-    list.items.forEach(item => {
+    const itemsInCart = [];
+    list.items.forEach((item) => {
       if (item.inCart) {
         itemsInCart.push(item);
       }
     });
-    await itemsInCart.forEach(item => {
-      item.userId = list.userId
-      item.itemId = item.item.id
-      delete item.item
-      item.measurementId = item.measurement.id
-      delete item.measurement
+    await itemsInCart.forEach((item) => {
+      item.userId = list.userId;
+      item.itemId = item.item.id;
+      delete item.item;
+      item.measurementId = item.measurement.id;
+      delete item.measurement;
 
-      dispatch(addItemToInventory(item))
-    })
-    await itemsInCart.forEach(item => {
-      dispatch(deleteShoppingListItem(item.id, shoppingListId))
-    })
-  }
+      dispatch(addItemToInventory(item));
+    });
+    await itemsInCart.forEach((item) => {
+      dispatch(deleteShoppingListItem(item.id, shoppingListId));
+    });
+  };
 
-  if(!loaded) return null
+  if (!loaded) return null;
   return (
     <div className="shoppingList__container">
-      <SearchBar pantry={false}/>
-      {list &&
+      <SearchBar pantry={false} />
+      {list && (
         <div>
-          <h1 className="shoppingList__title" >{list.name}</h1>
-          {categories.map(category => {
-            const categoryItems = list.items.filter(listItem => listItem.item.categoryId === category.id)
-            return <ShoppingListCategory key={category.id} category={category} items={categoryItems} />
+          <h1 className="shoppingList__title">{list.name}</h1>
+          {categories.map((category) => {
+            const categoryItems = list.items.filter(
+              (listItem) => listItem.item.categoryId === category.id
+            );
+            return (
+              <ShoppingListCategory key={category.id} category={category} items={categoryItems} />
+            );
           })}
-          {list.items.length > 0 &&
-          <button id="addToInv"
-            className="stdbutton"
-            onClick={transferList}>
-            Add crossed items to Inventory
+          {list.items.length > 0 && (
+            <button id="addToInv" className="stdbutton" onClick={transferList}>
+              Add items in cart to Inventory
             </button>
-          }
+          )}
         </div>
-      }
+      )}
     </div>
-  )
+  );
 }
