5f842afd4c7e78ecb6aa766d8df9c7f2b541af5f
Jon Wehner <jonjwehner@gmail.com>
2021-06-01 22:27:58 -0700

    fixing style errors in react app

--- react-app/src/store/session.js
+++ react-app/src/store/session.js
@@ -1,49 +1,45 @@
-const SET_USER = 'session/setUser'
-const REMOVE_USER = 'session/removeUser'
+const SET_USER = 'session/setUser';
+const REMOVE_USER = 'session/removeUser';
 
-const setUser = (user) => {
-  return {
-    type: SET_USER,
-    user
-  }
-}
-const removeUser = () => {
-  return {
-    type: REMOVE_USER,
-    user: null
-  }
-}
+const setUser = (user) => ({
+  type: SET_USER,
+  user,
+});
+const removeUser = () => ({
+  type: REMOVE_USER,
+  user: null,
+});
 export const login = (email, password) => async (dispatch) => {
   const response = await fetch('/api/auth/login', {
     method: 'POST',
     headers: {
-      'Content-Type': 'application/json'
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       email,
-      password
-    })
+      password,
+    }),
   });
-  const user = await response.json()
-  dispatch(setUser(user))
-  return user
-}
-
-export const logout = () => async (dispatch) => {
-  const response = await fetch("/api/auth/logout", {
-    headers: {
-      "Content-Type": "application/json",
-    }
-  });
-  dispatch(removeUser())
-  return await response.json();
+  const user = await response.json();
+  dispatch(setUser(user));
+  return user;
 };
 
-export const signUp = (username, email, password) => async (dispatch)  => {
-  const response = await fetch("/api/auth/signup", {
-    method: "POST",
+export const logout = () => async (dispatch) => {
+  const response = await fetch('/api/auth/logout', {
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
+    },
+  });
+  dispatch(removeUser());
+  return response.json();
+};
+
+export const signUp = (username, email, password) => async (dispatch) => {
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({
       username,
@@ -51,43 +47,43 @@ export const signUp = (username, email, password) => async (dispatch)  => {
       password,
     }),
   });
-  const user =  await response.json();
+  const user = await response.json();
   if (!user.errors) {
-    dispatch(setUser(user))
+    dispatch(setUser(user));
   }
-  return user
-}
+  return user;
+};
 
 export const authenticate = () => async (dispatch) => {
-  const response = await fetch('/api/auth/',{
+  const response = await fetch('/api/auth/', {
     headers: {
-      'Content-Type': 'application/json'
-    }
+      'Content-Type': 'application/json',
+    },
   });
   if (response.ok) {
-    const user = await response.json()
-    dispatch(setUser(user))
-    return user
+    const user = await response.json();
+    dispatch(setUser(user));
+    return user;
   }
-  return await response.json()
-}
+  return response.json();
+};
 
-const initialState = {user : null}
+const initialState = { user: null };
 
 const sessionReducer = (state = initialState, action) => {
   let newState;
   switch (action.type) {
     case SET_USER:
-      newState = {...state};
+      newState = { ...state };
       newState.user = action.user;
       return newState;
     case REMOVE_USER:
-      newState = {...state};
+      newState = { ...state };
       newState.user = action.user;
       return newState;
     default:
       return state;
-}
   }
+};
 
-  export default sessionReducer;
+export default sessionReducer;
--- react-app/src/store/shoppingList.js
+++ react-app/src/store/shoppingList.js
@@ -1,42 +1,36 @@
-const SET_SHOPPING_LISTS = 'shoppingLists/set'
-const EDIT_SHOPPING_LIST = 'shoppingLists/edit'
-const REMOVE_SHOPPING_LIST = 'shoppingLists/remove'
+const SET_SHOPPING_LISTS = 'shoppingLists/set';
+const EDIT_SHOPPING_LIST = 'shoppingLists/edit';
+const REMOVE_SHOPPING_LIST = 'shoppingLists/remove';
 
-const setShoppingLists = (shoppingLists) => {
-  return {
-    type: SET_SHOPPING_LISTS,
-    shoppingLists
-  }
-}
-const updateShoppingList = (shoppingList) => {
-  return {
-    type: EDIT_SHOPPING_LIST,
-    shoppingList
-  }
-}
+const setShoppingLists = (shoppingLists) => ({
+  type: SET_SHOPPING_LISTS,
+  shoppingLists,
+});
+const updateShoppingList = (shoppingList) => ({
+  type: EDIT_SHOPPING_LIST,
+  shoppingList,
+});
 
-const removeList = (id) => {
-  return {
-    type: REMOVE_SHOPPING_LIST,
-    id
-  }
-}
+const removeList = (id) => ({
+  type: REMOVE_SHOPPING_LIST,
+  id,
+});
 
-export const loadOneShoppingList = id => async (dispatch) => {
-  const res = await fetch(`/api/shopping-lists/${id}`)
+export const loadOneShoppingList = (id) => async (dispatch) => {
+  const res = await fetch(`/api/shopping-lists/${id}`);
   if (res.ok) {
-    const shoppingList = await res.json()
-    dispatch(setShoppingLists(shoppingList))
+    const shoppingList = await res.json();
+    dispatch(setShoppingLists(shoppingList));
   }
-}
+};
 export const loadUserShoppingLists = (userId) => async (dispatch) => {
   const res = await fetch(`/api/users/${userId}/shopping-lists/`);
   const shoppingLists = await res.json();
-  if (res.ok){
-    dispatch(setShoppingLists(shoppingLists.lists))
+  if (res.ok) {
+    dispatch(setShoppingLists(shoppingLists.lists));
   }
-  return shoppingLists
-}
+  return shoppingLists;
+};
 
 export const createShoppingList = (name, userId) => async (dispatch) => {
   const formData = new FormData();
@@ -45,17 +39,16 @@ export const createShoppingList = (name, userId) => async (dispatch) => {
   const options = {
     method: 'POST',
     body: formData,
-  }
+  };
   try {
     const res = await fetch('/api/shopping-lists/', options);
-    if (!res.ok) throw res
-    let shoppingList = await res.json()
-    if(!shoppingList.errors) {
-      dispatch(setShoppingLists(shoppingList))
+    if (!res.ok) throw res;
+    const shoppingList = await res.json();
+    if (!shoppingList.errors) {
+      dispatch(setShoppingLists(shoppingList));
     }
-    return shoppingList
-  }
-  catch (err){
+    return shoppingList;
+  } catch (err) {
     return err;
   }
 };
@@ -67,117 +60,114 @@ export const editShoppingList = (id, name, userId) => async (dispatch) => {
   const options = {
     method: 'PUT',
     body: formData,
-  }
+  };
   try {
     const res = await fetch(`/api/shopping-lists/${id}`, options);
-    if (!res.ok) throw res
-    const shoppingList = await res.json()
-    if(!shoppingList.errors) {
-      dispatch(updateShoppingList(shoppingList))
+    if (!res.ok) throw res;
+    const shoppingList = await res.json();
+    if (!shoppingList.errors) {
+      dispatch(updateShoppingList(shoppingList));
     }
-    return shoppingList
-  }
-  catch (err){
+    return shoppingList;
+  } catch (err) {
     return err;
   }
-}
+};
 
+// eslint-disable-next-line consistent-return
 export const deleteShoppingList = (id) => async (dispatch) => {
   const formData = new FormData();
-  formData.append('id', id)
+  formData.append('id', id);
   const options = {
     method: 'DELETE',
     body: formData,
-  }
+  };
   try {
-    const res = await fetch(`/api/shopping-lists/${id}`, options)
-    if (!res.ok) throw res
-    dispatch(removeList(id))
+    const res = await fetch(`/api/shopping-lists/${id}`, options);
+    if (!res.ok) throw res;
+    dispatch(removeList(id));
+  } catch (err) {
+    return err;
   }
-  catch (err) {
-    return err
-  }
-}
+};
 
-export const addEditShoppingListItem = (shoppingListItem) =>async (dispatch) => {
-  const { id, measurementId, quantity, shoppingListId, itemId, method } = shoppingListItem;
+export const addEditShoppingListItem = (shoppingListItem) => async (dispatch) => {
+  const {
+    id, measurementId, quantity, shoppingListId, itemId, method,
+  } = shoppingListItem;
   const formData = new FormData();
-  formData.append('item_id', itemId) ;
+  formData.append('item_id', itemId);
   formData.append('measurement_id', measurementId);
   formData.append('quantity', quantity);
   const options = {
     method,
-    body: formData
+    body: formData,
   };
   let url;
-  if(method === 'POST') {
-    url = `/api/shopping-lists/${shoppingListId}/items`
-  }
-  else {
-    url = `/api/shopping-lists/${shoppingListId}/items/${id}`
+  if (method === 'POST') {
+    url = `/api/shopping-lists/${shoppingListId}/items`;
+  } else {
+    url = `/api/shopping-lists/${shoppingListId}/items/${id}`;
   }
 
   try {
     const res = await fetch(url, options);
     if (!res.ok) throw res;
-    const newShoppingList = await res.json()
-    if(!newShoppingList.errors) {
-      dispatch(setShoppingLists(newShoppingList))
+    const newShoppingList = await res.json();
+    if (!newShoppingList.errors) {
+      dispatch(setShoppingLists(newShoppingList));
     }
-    return newShoppingList
+    return newShoppingList;
+  } catch (err) {
+    return err;
   }
-  catch (err) {
-    return err
-  }
-}
+};
 
 export const deleteShoppingListItem = (id, shoppingListId) => async (dispatch) => {
-  const url = `/api/shopping-lists/${shoppingListId}/items/${id}`
+  const url = `/api/shopping-lists/${shoppingListId}/items/${id}`;
   const options = {
-    method: 'DELETE'
-  }
+    method: 'DELETE',
+  };
   try {
-    const res = await fetch(url, options)
-    if (!res.ok) throw res
-    const shoppingList = await res.json()
-    dispatch(setShoppingLists(shoppingList))
-    return shoppingList
-  }
-  catch (err) {
-    return err
+    const res = await fetch(url, options);
+    if (!res.ok) throw res;
+    const shoppingList = await res.json();
+    dispatch(setShoppingLists(shoppingList));
+    return shoppingList;
+  } catch (err) {
+    return err;
   }
 };
 
-export const addRemoveCart =(id, shoppingListId) => async (dispatch) => {
-  const url=`/api/shopping-lists/${shoppingListId}/items/${id}`
+export const addRemoveCart = (id, shoppingListId) => async (dispatch) => {
+  const url = `/api/shopping-lists/${shoppingListId}/items/${id}`;
   const options = {
     method: 'PATCH',
-  }
+  };
   try {
-    const res = await fetch(url, options)
-    if(!res.ok) throw res
-    const shoppingList = await res.json()
-    dispatch(setShoppingLists(shoppingList))
-    return shoppingList
-  }
-  catch (err) {
-    return err
+    const res = await fetch(url, options);
+    if (!res.ok) throw res;
+    const shoppingList = await res.json();
+    dispatch(setShoppingLists(shoppingList));
+    return shoppingList;
+  } catch (err) {
+    return err;
   }
 };
 
-const initialState = {}
+const initialState = {};
 const shoppingListReducer = (state = initialState, action) => {
-  let newState = {}
-  switch(action.type) {
+  let newState = {};
+  switch (action.type) {
     case SET_SHOPPING_LISTS:
-      newState = {...state, ...action.shoppingLists};
+      newState = { ...state, ...action.shoppingLists };
       return newState;
     case EDIT_SHOPPING_LIST:
-      newState = {...state};
+      newState = { ...state };
       newState[action.shoppingList.id] = action.shoppingList;
       return newState;
     case REMOVE_SHOPPING_LIST:
-      newState = {...state};
+      newState = { ...state };
       delete newState[action.id];
       return newState;
     default:
@@ -185,4 +175,4 @@ const shoppingListReducer = (state = initialState, action) => {
   }
 };
 
-export default shoppingListReducer
+export default shoppingListReducer;
