af88e9f54206246a2a7a0790821e6378044b46eb
Jon Wehner <jonjwehner@gmail.com>
2022-02-03 23:22:34 -0800

    added route parameter to shopping list route

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -48,7 +48,7 @@ function App() {
             )}
         />
         <Route
-          path="/shopping-lists"
+          path="/shopping-lists/:id"
           element={(
             <ShoppingList />
           )}
