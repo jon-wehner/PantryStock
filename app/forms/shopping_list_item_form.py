ddceeb4a89f6330369758eb5bf19db1cc05b1dfd
Jon Wehner <jonjwehner@gmail.com>
2022-03-16 17:16:03 -0700

    shopping list errors

--- app/forms/shopping_list_item_form.py
+++ app/forms/shopping_list_item_form.py
@@ -5,6 +5,7 @@ from wtforms.validators import DataRequired
 
 class ShoppingListItemForm(FlaskForm):
     item_id = IntegerField('item_id', validators=[DataRequired()])
-    measurement_id = IntegerField('measurement_id',
-                                  validators=[DataRequired()])
-    quantity = IntegerField('quantity', validators=[DataRequired()])
+    measurement_id = IntegerField('measurement_id', validators=[
+        DataRequired('Please Choose a package size')])
+    quantity = IntegerField('quantity', validators=[
+        DataRequired('Enter a quantity')])
