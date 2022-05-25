f89b0c9f6e38107e20875e54af896399b61e8df5
Jon Wehner <jonjwehner@gmail.com>
2022-05-24 17:44:06 -0700

    tests working

--- react-app/tests/login.spec.ts
+++ react-app/tests/login.spec.ts
@@ -1,9 +1,11 @@
 import { test, expect, Page } from '@playwright/test';
 
 test.describe('Login', () => {
-  test('user should be able to navigate to the login page', async ({ page }) => {
-    await page.goto('http://127.0.0.1:3000')
-    await page.locator('text=Login').click()
-    await expect(page.locator('text=Email'))
+  test.beforeEach(async ({ page }) => {
+    await page.goto('http://127.0.0.1:3000');
+    await page.locator('text=Login').click();
+  })
+  test('user should be able to navigate to the login page', async ({ page }) => {    
+    await expect(page.locator('text=Email').first()).toBeVisible();
 
 })})
