6e68fb2d1890e3f45d067f0027ed140bc2031cdc
Jon Wehner <jonjwehner@gmail.com>
2022-06-14 23:47:32 -0700

    store update

--- react-app/src/store/index.ts
+++ react-app/src/store/index.ts
@@ -1,8 +1,5 @@
 /* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable global-require */
-import {
-  createStore, combineReducers, applyMiddleware, compose,
-} from 'redux';
+import { compose } from 'redux';
 import { configureStore } from '@reduxjs/toolkit';
 import { logger } from 'redux-logger';
 import sessionReducer from './session';
@@ -11,7 +8,12 @@ import shoppingListReducer from './shoppingList';
 import inventoryReducer from './inventory';
 import categoryReducer from './category';
 
-
+declare global {
+  // eslint-disable-next-line no-unused-vars
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENTSION_COMPOSE__?: typeof compose
+  }
+}
 
 export const reducer = {
   session: sessionReducer,
@@ -21,10 +23,11 @@ export const reducer = {
   categories: categoryReducer,
 };
 
-export const store = configureStore({ 
+export const store = configureStore({
   reducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
   devTools: process.env.NODE_ENV !== 'production',
 });
 
-
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
