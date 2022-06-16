a786bd5e94480eb281586df8b38249d01d5fb8bd
Jon Wehner <jonjwehner@gmail.com>
2022-06-16 15:04:54 -0700

    added testing for days to expire

--- react-app/src/__tests__/utils.test.js
+++ react-app/src/__tests__/utils.test.js
@@ -3,13 +3,17 @@ import { getDaysToExpire, getTimeStamp } from '../services/utils';
 describe('getTimeStamp Function', () => {
   test('Given a date string, returns correct string in zulu time', () => {
     const testString = '2022-06-16';
-    const adjustedString = '2022-06-16T07:00:00.000Z';
-    expect(getTimeStamp(testString)).toEqual(adjustedString);
+    const adjString = '2022-06-16T07:00:00.000Z';
+    expect(getTimeStamp(testString)).toEqual(adjString);
   });
 });
 
 describe('the days to expire function', () => {
   test('given a date string it returns the correct days to expire', () => {
-    const daysToExpire = getDaysToExpire('2022-06-15 07:00:00');
+    const twoDaysFromNow = new Date();
+    twoDaysFromNow.setDate(twoDaysFromNow.getDate() + 2);
+    const isoDate = new Date(twoDaysFromNow).toISOString();
+    const daysToExpiration = getDaysToExpire(new Date(isoDate));
+    expect(daysToExpiration).toEqual(2);
   });
 });
--- react-app/src/services/utils.ts
+++ react-app/src/services/utils.ts
@@ -1,6 +1,6 @@
 export const getDaysToExpire = (date: string) => {
   const expiration = new Date(date);
-  const daysToExpire = Math.ceil((expiration.getTime() - new Date().getTime()) / (1000 * 360 * 60 * 24));
+  const daysToExpire = Math.ceil((expiration.getTime() - new Date().getTime()) / (1000 * 3600 * 24));
   return daysToExpire;
 };
 
